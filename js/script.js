813c48a568d53c2627626d61f71426f0ae4cc326
MAbdurahman <mdbdrrhm@gmail.com>
2021-10-21 12:30:45 -0500

    Create, style, and animate navbar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 /*-----Javascript for js-business-website */
 $(document).ready(function () {
-	// Navbar
+	
+	/*=============================================
+            navbar
+================================================*/
 	const menu = document.querySelector('.menu');
 	const navbar = document.querySelector('.navbar');
 
@@ -8,9 +11,10 @@ $(document).ready(function () {
 		navbar.classList.toggle('change');
 		menu.classList.toggle('change');
 	});
-	// End of Navbar
 
-	// Section 2 Video
+	/*=============================================
+            section 2 video
+================================================*/
 	const video = document.querySelector('.video');
 	const btn = document.querySelector('.buttons button i');
 	const bar = document.querySelector('.video-bar');
@@ -40,7 +44,10 @@ $(document).ready(function () {
 			video.style.opacity = '.3';
 		}
 	});
-	// End of Section 2 Video
+
+	/*=============================================
+            swiper
+================================================*/
 
 	var swiper = new Swiper('.swiper-container', {
 		effect: 'coverflow',
